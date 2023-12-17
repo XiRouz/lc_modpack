7cd420bc3db509f20957fd5dc0a3a7565bc2c860
Lavrentev Semyon <xirouz@gmail.com>
2023-12-17 15:19:24 +0300

    updated script

--- UpdateModpack.bat
+++ UpdateModpack.bat
@@ -1,17 +1,17 @@
 @echo off
 winget install --id Git.Git -e --source winget
 
-IF EXIST ".git\" (
+IF EXIST "git\" (
     echo repository exists
 ) ELSE (
-    IF EXIST ".BepInEx\" (
+    IF EXIST "BepInEx\" (
         rd /s /q "BepInEx"
     )
     git clone https://github.com/XiRouz/lc_modpack.git
     echo repository cloned
     robocopy "%cd%\lc_modpack" "%cd%" /E
 )
-IF EXIST ".lc_modpack\" (
+IF EXIST "lc_modpack\" (
     echo git modpack folder delete
     rd /s /q "lc_modpack"
 )
